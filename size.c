d14680d5c5fca6208942b1a36cccfe2d2bd2cb16
AMDINDOWS <30437892+AMDINDOWS@users.noreply.github.com>
2022-03-21 21:14:25 +0530

    Update
    Update

---
+++ size.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    printf("size of char : %d \n", sizeof(char));
+    printf("size of int : %d \n", sizeof(int));
+    printf("size of unsigned int : %d \n", sizeof(unsigned int));
+    printf("size of long : %d \n", sizeof(long));
+    printf("size of long long : %d \n", sizeof(long long));
+    printf("size of double : %d \n", sizeof(double));
+    printf("size of long double : %d \n", sizeof(long double));
+    
+}
